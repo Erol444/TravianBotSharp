1b318f859c23591d4bb13ce448a1ea2204ac03b2
Erol444 <erol123444@gmail.com>
2020-12-29 00:55:19 +0100

    add (driverhelper) added helper functions for driverHelper

--- TbsCore/Helpers/DriverHelper.cs
+++ TbsCore/Helpers/DriverHelper.cs
@@ -25,11 +25,37 @@ namespace TbsCore.Helpers
                 acc.Wb.Html.LoadHtml(acc.Wb.Driver.PageSource);
                 return true;
             }
-            catch(Exception e) 
+            catch (Exception e)
             {
                 if (log) acc.Wb.Log($"Error executing JS script:\n{script}", e);
                 return false;
             }
         }
+
+        public static async Task<bool> ClickById(Account acc, string query) =>
+            await ExecuteAction(acc, new QueryById(query), new ActionClick());
+        public static async Task<bool> WriteById(Account acc, string query, object text) =>
+            await ExecuteAction(acc, new QueryById(query), new ActionWrite(text));
+        public static async Task<bool> ClickByClassName(Account acc, string query) =>
+            await ExecuteAction(acc, new QueryByClassName(query), new ActionClick());
+        public static async Task<bool> WriteByClassName(Account acc, string query, object text) =>
+            await ExecuteAction(acc, new QueryByClassName(query), new ActionWrite(text));
+        public static async Task<bool> ClickByName(Account acc, string query) =>
+            await ExecuteAction(acc, new QueryByName(query), new ActionClick());
+        public static async Task<bool> WriteByName(Account acc, string query, object text) =>
+            await ExecuteAction(acc, new QueryByName(query), new ActionWrite(text));
+
+        private static async Task<bool> ExecuteAction(Account acc, Query query, Action action, bool log = true) =>
+            await ExecuteScript(acc, $"document.{query.val}{action.val}", log);
+
+        
+        public class QueryById : Query { public QueryById(string str) => base.val = $"getElementById('{str}')"; }
+        public class QueryByName : Query { public QueryByName(string str) => base.val = $"getElementsByName('{str}')[0]"; }
+        public class QueryByClassName : Query { public QueryByClassName(string str) => base.val = $"getElementsByClassName('{str}')[0]"; }
+        public class ActionWrite : Action { public ActionWrite(object str) => base.val = $".value='{str}';"; }
+        public class ActionClick : Action { public ActionClick() => base.val = ".click();"; }
+
+        public abstract class Action { public string val; }
+        public abstract class Query { public string val; }
     }
 }
