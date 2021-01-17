1452eb2176d04cba0546fac53cb1d03bbb4cccec
Erol444 <erol123444@gmail.com>
2021-01-17 01:56:49 +0100

    fix (claim task rewards NYS) fixed claiming task rewards on NYS servers. Now supports claiming acc-wide rewards as well (previously it was just looping)

--- TbsCore/Tasks/LowLevel/ClaimBeginnerTask2021.cs
+++ TbsCore/Tasks/LowLevel/ClaimBeginnerTask2021.cs
@@ -11,18 +11,33 @@ namespace TravBotSharp.Files.Tasks.LowLevel
     {
         public override async Task<TaskRes> Execute(Account acc)
         {
+            // Claim village-wide rewards
             await acc.Wb.Navigate($"{acc.AccInfo.ServerUrl}/tasks");
+            await ClaimRewards(acc);
 
+            if (acc.Wb.Html
+                .GetElementbyId("sidebarBoxQuestmaster")?
+                .Descendants()?
+                .Any(x => x.HasClass("newQuestSpeechBubble")) ?? false)
+            {
+                // Claim account-wide rewards
+                await acc.Wb.Navigate($"{acc.AccInfo.ServerUrl}/tasks?t=2");
+                await ClaimRewards(acc);
+            }
+
+                TaskExecutor.AddTask(acc, new HeroUpdateInfo() { ExecuteAt = DateTime.Now });
+
+            return TaskRes.Executed;
+        }
+
+        private async Task ClaimRewards(Account acc)
+        {
             await Task.Delay(AccountHelper.Delay());
             do
             {
-                await DriverHelper.ClickByClassName(acc, "collect");
+                await DriverHelper.ClickByClassName(acc, "collect", false);
             }
             while (acc.Wb.Html.DocumentNode.Descendants("button").Any(x => x.HasClass("collect")));
-
-            TaskExecutor.AddTask(acc, new HeroUpdateInfo() { ExecuteAt = DateTime.Now });
-
-            return TaskRes.Executed;
         }
     }
 }
