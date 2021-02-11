a08508aaadf4bcfd51d686276c649c9028f37f5f
VINAGHOST <nguyentrungnhan87@gmail.com>
2021-02-11 13:47:15 +0700

    add find code server

--- TravBotSharp/Views/FarmFinderUc.Designer.cs
+++ TravBotSharp/Views/FarmFinderUc.Designer.cs
@@ -30,6 +30,7 @@ namespace TravBotSharp
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -41,10 +42,22 @@ namespace TravBotSharp
             this.label1.TabIndex = 0;
             this.label1.Text = "Suggest farm";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(155, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(382, 178);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "The bigger the better";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // FarmFinderUc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "FarmFinderUc";
             this.Size = new System.Drawing.Size(766, 504);
@@ -56,5 +69,6 @@ namespace TravBotSharp
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- TravBotSharp/Views/FarmFinderUc.cs
+++ TravBotSharp/Views/FarmFinderUc.cs
@@ -1,13 +1,26 @@
-﻿using TravBotSharp.Views;
+﻿using System;
+using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+
+using RestSharp;
+using HtmlAgilityPack;
+
+using TbsCore.Models.AccModels;
+
+using TravBotSharp.Views;
 using TravBotSharp.Interfaces;
 
 namespace TravBotSharp
 {
     public partial class FarmFinderUc : TbsBaseUc, ITbsUc
     {
+        private RestClient client;
+
         public FarmFinderUc()
         {
             InitializeComponent();
+            client = new RestClient("https://travianstats.de/index.php");
         }
 
         public void Init()
@@ -16,6 +29,44 @@ namespace TravBotSharp
 
         public void UpdateUc()
         {
+        }
+
+        /// <summary>
+        /// Server code for travianstats.de
+        /// </summary>
+        /// <returns></returns>
+        private async Task<string> GetServerCode(Account acc)
+        {
+            //get serverUrl without https://
+            var url = (new UriBuilder(acc.AccInfo.ServerUrl)).Host;
+
+            //request to travaianstats.de
+            var request = new RestRequest("", DataFormat.None);
+            var response = await client.ExecuteAsync(request);
+
+            if (response.StatusCode != HttpStatusCode.OK) throw new Exception("SendGetReq failed!\n" + response.Content);
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(response.Content);
+            // use this form to search code of our server
+            var form = doc.GetElementbyId("welt");
+
+            // find our server
+            foreach (var node in form.Descendants())
+            {
+                if (node.InnerText.Contains(url))
+                {
+                    return node.Attributes["value"].Value;
+                }
+            }
+
+            return "404";
+        }
+
+        private async void button1_Click(object sender, System.EventArgs e)
+        {
+            var acc = GetSelectedAcc();
+            acc.Wb.Log(await GetServerCode(acc));
         }
     }
 }
