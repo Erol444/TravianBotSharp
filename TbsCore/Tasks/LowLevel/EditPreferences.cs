dba3c267356b16a7b7853082913123581d1ef8d5
Erol444 <erol123444@gmail.com>
2021-02-12 01:24:47 +0100

    add (edit preferences) added edit preferences task. currently supports disabling contextual help and editing TroopsPerPage count

--- TbsCore/Tasks/LowLevel/EditPreferences.cs
+++ TbsCore/Tasks/LowLevel/EditPreferences.cs
@@ -5,12 +5,27 @@ using TbsCore.Models.AccModels;
 
 namespace TravBotSharp.Files.Tasks.LowLevel
 {
-    public class DisableContextualHelp : BotTask
+    public class EditPreferences : BotTask
     {
+        /// <summary>
+        /// Disable contextual help
+        /// </summary>
+        public bool? ContextualHelp { get; set; }
+        /// <summary>
+        /// Troop movements per page in rally point
+        /// </summary>
+        public int? TroopsPerPage { get; set; }
+
         public override async Task<TaskRes> Execute(Account acc)
         {
             await acc.Wb.Navigate($"{acc.AccInfo.ServerUrl}/options");
-            await DriverHelper.ClickById(acc, "v13");
+
+            if(ContextualHelp != null)
+                await DriverHelper.CheckById(acc, "v13", ContextualHelp ?? true);
+
+            if (TroopsPerPage != null)
+                await DriverHelper.WriteById(acc, "troopMovementsPerPage", TroopsPerPage ?? 10);
+
 
             var acceptButton = acc.Wb.Html.DocumentNode
                 .Descendants("div")
