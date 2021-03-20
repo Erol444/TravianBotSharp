95da182447a3db7fece233a6ac8fdf22163ba148
VINAGHOST <nguyentrungnhan87@gmail.com>
2021-03-21 00:37:33 +0700

    list account: vue material -> bootstrap vue

--- TBSWeb/ClientApp/src/components/Accounts/ListAccount.vue
+++ TBSWeb/ClientApp/src/components/Accounts/ListAccount.vue
@@ -1,23 +1,13 @@
 <template>
-    <b-card no-body>
-        <md-table
-            v-model="accs"
-            md-card
-            md-fixed-header
-        >
-            <md-table-row
-                slot="md-table-row"
-                slot-scope="{ item }"
-            >
-                <md-table-cell md-label="username">
-                    {{ item.username }}
-                </md-table-cell>
-                <md-table-cell md-label="server">
-                    {{ item.server }}
-                </md-table-cell>
-            </md-table-row>
-        </md-table>
-    </b-card>
+    <div>
+        <b-table
+            responsive
+            sticky-header
+            striped
+            hover
+            :items="accs"
+        />
+    </div>
 </template>
 
 <script>
@@ -25,59 +15,13 @@
     export default {
         name: 'ListAccount',
         data: () => ({
+            fields: ['username', 'server'],
             accs: [
-                {
-                    username: 'vinaghost',
-                    server: 'ts4',
-                },
-                {
-                    username: 'adasdsad',
-                    server: 'tsasdas4',
-                },
-                {
-                    username: 'vinsadsadsadaghost',
-                    server: 'tssadsa4',
-                },
-                {
-                    username: 'vinsadsadsadaghost',
-                    server: 'tssadsa4',
-                },
-                {
-                    username: 'vinsadsadsadaghost',
-                    server: 'tssadsa4',
-                },
-                {
-                    username: 'vinsadsadsadaghost',
-                    server: 'tssadsa4',
-                },
-                {
-                    username: 'vinsadsadsadaghost',
-                    server: 'tssadsa4',
-                },
-                {
-                    username: 'vinsadsadsadaghost',
-                    server: 'tssadsa4',
-                },
-                {
-                    username: 'vinsadsadsadaghost',
-                    server: 'tssadsa4',
-                },
-                {
-                    username: 'vinsadsadsadaghost',
-                    server: 'tssadsa4',
-                },
-                {
-                    username: 'vinsadsadsadaghost',
-                    server: 'tssadsa4',
-                },
-                {
-                    username: 'vinsadsadsadaghost',
-                    server: 'tssadsa4',
-                },
-                {
-                    username: 'vinsadsadsadaghost',
-                    server: 'tssadsa4',
-                },
+                { username: 'vinaghost', server: 'ts4' },
+                { username: 'adasdsad', server: 'tsasdas4' },
+                { username: 'adasdsad', server: 'tsasdas4' },
+                { username: 'adasdsad', server: 'tsasdas4' },
+                { username: 'adasdsad', server: 'tsasdas4' },
             ],
         }),
     };
