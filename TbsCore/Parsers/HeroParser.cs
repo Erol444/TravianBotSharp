78465e0bb2259e4d420453e7e874e782628aff87
Finfinder <rtrzepocki@gmail.com>
2021-10-29 16:22:20 +0200

    Update HeroParser.cs
    Fix incorrect decimal separator - Different cultures use different decimal separators

--- TbsCore/Parsers/HeroParser.cs
+++ TbsCore/Parsers/HeroParser.cs
@@ -223,8 +223,8 @@ namespace TbsCore.Parsers
                     var path = htmlDoc.GetElementbyId("healthMask").Descendants("path").FirstOrDefault();
                     if (path == null) return 0;
                     var commands = path.GetAttributeValue("d", "").Split(' ');
-                    var xx = double.Parse(commands[commands.Length - 2]);
-                    var yy = double.Parse(commands[commands.Length - 1]);
+                    var xx = double.Parse(commands[commands.Length - 2], System.Globalization.CultureInfo.InvariantCulture);
+                    var yy = double.Parse(commands[commands.Length - 1], System.Globalization.CultureInfo.InvariantCulture);
 
                     var rad = Math.Atan2(yy - 55, xx - 55);
                     return (int)Math.Round(-56.173 * rad + 96.077);
