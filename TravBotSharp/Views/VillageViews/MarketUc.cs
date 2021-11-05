5b9f3f3ccfcb7ccdb4d5e64a23289a916ec9cf7e
VINAGHOST <nguyentrungnhan87@gmail.com>
2021-11-05 15:02:55 +0700

    change UI in Village/MarketUc

--- TravBotSharp/Views/VillageViews/MarketUc.cs
+++ TravBotSharp/Views/VillageViews/MarketUc.cs
@@ -1,106 +1,152 @@
 ﻿using System;
+using System.Linq;
 using TravBotSharp.Interfaces;
+using TbsCore.Models.VillageModels;
+using TbsCore.Models.ResourceModels;
+using TbsCore.Models.MapModels;
+using TbsCore.Tasks.LowLevel;
+using TbsCore.Tasks.SecondLevel;
 
 namespace TravBotSharp.Views
 {
     public partial class MarketUc : BaseVillageUc, ITbsUc
     {
+        public Resources _res;
+
         public MarketUc()
         {
             InitializeComponent();
+            _res = new Resources();
+            numericUpDown6.Maximum = long.MaxValue;
+            numericUpDown7.Maximum = long.MaxValue;
+            numericUpDown8.Maximum = long.MaxValue;
+            numericUpDown9.Maximum = long.MaxValue;
         }
 
         public void UpdateUc()
         {
             var vill = GetSelectedVillage();
-            TargetLimitWood.Value = vill.Market.Settings.Configuration.TargetLimit.Wood;
-            TargetLimitClay.Value = vill.Market.Settings.Configuration.TargetLimit.Clay;
-            TargetLimitIron.Value = vill.Market.Settings.Configuration.TargetLimit.Iron;
-            TargetLimitCrop.Value = vill.Market.Settings.Configuration.TargetLimit.Crop;
-            FillLimitWood.Value = vill.Market.Settings.Configuration.FillLimit.Wood;
-            FillLimitClay.Value = vill.Market.Settings.Configuration.FillLimit.Clay;
-            FillLimitIron.Value = vill.Market.Settings.Configuration.FillLimit.Iron;
-            FillLimitCrop.Value = vill.Market.Settings.Configuration.FillLimit.Crop;
-            transitResEnabled.Checked = vill.Market.Settings.Configuration.Enabled;
-            TransitArrival.Text = vill.Market.Settings.Configuration.TransitArrival.ToString();
-            LastTransit.Text = vill.Market.LastTransit.ToString();
-            //Send res to main vill config
-            woodSend.Value = vill.Market.Settings.Configuration.SendResLimit.Wood;
-            claySend.Value = vill.Market.Settings.Configuration.SendResLimit.Clay;
-            ironSend.Value = vill.Market.Settings.Configuration.SendResLimit.Iron;
-            cropSend.Value = vill.Market.Settings.Configuration.SendResLimit.Crop;
 
-            //For npc
-            npcEnabled.Checked = vill.Market.Npc.Enabled;
-            overflowProtection.Checked = vill.Market.Npc.NpcIfOverflow;
-            numericUpDown4.Value = vill.Market.Npc.ResourcesRatio.Wood;
-            numericUpDown3.Value = vill.Market.Npc.ResourcesRatio.Clay;
-            numericUpDown2.Value = vill.Market.Npc.ResourcesRatio.Iron;
-            numericUpDown1.Value = vill.Market.Npc.ResourcesRatio.Crop;
+            var acc = GetSelectedAcc();
+            if (acc == null) return;
+
+            VillageComboBox.Items.Clear();
+            foreach (var _vill in acc.Villages)
+            {
+                if (_vill.Id == vill.Id) continue;
+                VillageComboBox.Items.Add(_vill.Name);
+            }
+            if (VillageComboBox.Items.Count > 0)
+            {
+                VillageComboBox.SelectedIndex = 0;
+            }
+
+            richTextBox1.Text = vill.Res.Stored.Resources.ToString();
+            //vill.Market.Merchant = vill.Market.Merchant ?? new MerchantInfo();
+            //MerchantInfo.Text = $"Merchant: {vill.Market.Merchant.Free}/ {vill.Market.Merchant.Number} ({vill.Market.Merchant.Capacity})";
         }
 
-        private void transitResEnabled_CheckedChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Settings.Configuration.Enabled = transitResEnabled.Checked;
+        private void UpdateMerchantNeed(Village vill)
+        {
+            //var merchant = vill.Market.Merchant;
+            //if (merchant.Capacity > 0)
+            //{
+            //    var reminder = _res.Sum() % merchant.Capacity;
+            //    if (reminder > 0)
+            //    {
+            //        label19.Text = $"Need {_res.Sum() / merchant.Capacity + 1} merchant";
+            //    }
+            //    else
+            //    {
+            //        label19.Text = $"Need {_res.Sum() / merchant.Capacity} merchant";
+            //    }
+            //}
+        }
 
-        private void overflowProtection_CheckedChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Npc.NpcIfOverflow = overflowProtection.Checked;
+        #region Target Village Callback
 
-        private void numericUpDown4_ValueChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Npc.ResourcesRatio.Wood = (long)numericUpDown4.Value;
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.VillageComboBox.Enabled = radioButton1.Checked;
+        }
 
-        private void numericUpDown3_ValueChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Npc.ResourcesRatio.Clay = (long)numericUpDown3.Value;
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            this.coordinatesUc1.Enabled = radioButton2.Checked;
+        }
 
-        private void numericUpDown2_ValueChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Npc.ResourcesRatio.Iron = (long)numericUpDown2.Value;
+        #endregion Target Village Callback
 
-        private void numericUpDown1_ValueChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Npc.ResourcesRatio.Crop = (long)numericUpDown1.Value;
+        //private void button2_Click(object sender, EventArgs e) =>
+        //    GetSelectedAcc().Tasks.Add(
+        //        new UpdateMarket
+        //        {
+        //            ExecuteAt = DateTime.Now.AddHours(-10),
+        //            Vill = GetSelectedVillage(),
+        //        },
+        //        true,
+        //        GetSelectedVillage());
 
-        private void npcEnabled_CheckedChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Npc.Enabled = npcEnabled.Checked;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Coordinates coord;
+            if (radioButton1.Checked)
+            {
+                if (VillageComboBox.SelectedIndex == -1) return;
+                coord = GetSelectedAcc().Villages.FirstOrDefault(x => x.Name == VillageComboBox.Items[VillageComboBox.SelectedIndex].ToString()).Coordinates;
+            }
+            else
+            {
+                coord = coordinatesUc1.Coords;
+            }
+            GetSelectedAcc().Tasks.Add(
+                new SendResources
+                {
+                    ExecuteAt = DateTime.Now.AddHours(-10),
+                    Vill = GetSelectedVillage(),
+                    Resources = _res,
+                    Coordinates = coord,
+                    RunTimes = 1,
+                },
+                true,
+                GetSelectedVillage());
+        }
 
-        #region SendMainVill Callbacks
-        private void woodSend_ValueChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Settings.Configuration.SendResLimit.Wood = (long)woodSend.Value;
+        private void numericUpDown9_ValueChanged(object sender, EventArgs e)
+        {
+            _res.Wood = (long)numericUpDown9.Value;
+            UpdateMerchantNeed(GetSelectedVillage());
+        }
 
-        private void claySend_ValueChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Settings.Configuration.SendResLimit.Clay = (long)claySend.Value;
+        private void numericUpDown8_ValueChanged(object sender, EventArgs e)
+        {
+            _res.Clay = (long)numericUpDown8.Value;
+            UpdateMerchantNeed(GetSelectedVillage());
+        }
 
-        private void ironSend_ValueChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Settings.Configuration.SendResLimit.Iron = (long)ironSend.Value;
+        private void numericUpDown7_ValueChanged(object sender, EventArgs e)
+        {
+            _res.Iron = (long)numericUpDown7.Value;
+            UpdateMerchantNeed(GetSelectedVillage());
+        }
 
-        private void cropSend_ValueChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Settings.Configuration.SendResLimit.Crop = (long)cropSend.Value;
-        #endregion
+        private void numericUpDown6_ValueChanged(object sender, EventArgs e)
+        {
+            _res.Crop = (long)numericUpDown6.Value;
+            UpdateMerchantNeed(GetSelectedVillage());
+        }
 
-        #region TargetLimit Callbacks
-        private void TargetLimitWood_ValueChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Settings.Configuration.TargetLimit.Wood = (long)TargetLimitWood.Value;
-
-        private void TargetLimitClay_ValueChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Settings.Configuration.TargetLimit.Clay = (long)TargetLimitClay.Value;
-
-        private void TargetLimitIron_ValueChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Settings.Configuration.TargetLimit.Iron = (long)TargetLimitIron.Value;
-
-        private void TargetLimitCrop_ValueChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Settings.Configuration.TargetLimit.Crop = (long)TargetLimitCrop.Value;
-        #endregion
-
-        #region FillLimit Callbacks
-        private void FillLimitWood_ValueChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Settings.Configuration.FillLimit.Wood = (long)FillLimitWood.Value;
-
-        private void FillLimitClay_ValueChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Settings.Configuration.FillLimit.Clay = (long)FillLimitClay.Value;
-
-        private void FillLimitIron_ValueChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Settings.Configuration.FillLimit.Iron = (long)FillLimitIron.Value;
-
-        private void FillLimitCrop_ValueChanged(object sender, EventArgs e) =>
-            GetSelectedVillage().Market.Settings.Configuration.FillLimit.Crop = (long)FillLimitCrop.Value;
-        #endregion
-
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var acc = GetSelectedAcc();
+            var vill = GetSelectedVillage();
+            acc.Tasks.Remove(typeof(NPC), vill);
+            acc.Tasks.Add(
+                new NPC()
+                {
+                    ExecuteAt = DateTime.Now,
+                    Vill = vill,
+                });
+        }
     }
 }
