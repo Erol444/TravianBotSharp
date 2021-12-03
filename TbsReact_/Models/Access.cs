57099636c2566e9860bcd86a981536aa3358c3d0
VINAGHOST <nguyentrungnhan87@gmail.com>
2021-12-03 19:55:07 +0700

    remove abundance folder&file

--- TbsReact_/Models/Access.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TbsReact.Models
-{
-    public class Access
-    {
-        public int Id { get; set; }
-        public string Password { get; set; }
-        public Proxy Proxy { get; set; }
-    }
-}
