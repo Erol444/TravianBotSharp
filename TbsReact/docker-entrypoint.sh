3638f541496f670ab18561e403243598db86dacc
VINAGHOST <nguyentrungnhan87@gmail.com>
2021-12-16 14:35:20 +0700

    a

--- TbsReact/docker-entrypoint.sh
+++ TbsReact/docker-entrypoint.sh
@@ -15,17 +15,17 @@ if [ ! -z "$XFB_SCREEN_DPI" ]; then
 fi
 
 # cleanup previous executions
-rm -fr ~/xvfb.log /tmp/.X11-unix/ /tmp/.X0-lock /Xauthority
+rm -fr ~/xvfb.log /tmp/.X11-unix/ /tmp/.X0-lock /.Xauthority
 
 # first we need our security cookie and add it to user's .Xauthority
 mcookie | sed -e 's/^/add :0 MIT-MAGIC-COOKIE-1 /' | xauth -q
 
 # now place the security cookie with FamilyWild on volume so client can use it
 # see http://stackoverflow.com/25280523 for details on the following command
-xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f /Xauthority/xserver.xauth nmerge -
+xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f /.Xauthority/xserver.xauth nmerge -
 
 # now boot X-Server, tell it to our cookie and give it sometime to start up
-Xvfb :0 -auth ~/Xauthority $DPI_OPTIONS -screen 0 $XFB_SCREEN >>~/xvfb.log 2>&1 &
+Xvfb :0 -auth ~/.Xauthority $DPI_OPTIONS -screen 0 $XFB_SCREEN >>~/xvfb.log 2>&1 &
 sleep 10
 
 google-chrome --disable-gpu --no-sandbox --disable-setuid-sandbox https://www.google.com.vn/
