ecb5c4bb37268456797245bf47f6419ebf3c7758
VINAGHOST <nguyentrungnhan87@gmail.com>
2022-02-20 14:31:42 +0700

    [TBSReact] Add draggable table for queue building

--- TbsReact/ClientApp/src/components/Views/VillagesView/BuildView/QueueBuilding.js
+++ TbsReact/ClientApp/src/components/Views/VillagesView/BuildView/QueueBuilding.js
@@ -1,28 +1,45 @@
 import { Typography } from "@mui/material";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ContentBox from "../../../ContentBox";
-import MUITable from "../../../ref/MUITable";
+import MUIDraggableTable from "../../../ref/MUIDraggableTable";
+import { arrayMoveImmutable } from "array-move";
 
 const QueueBuilding = () => {
 	const header = ["Building", "Level", "Location"];
 	const data = [
 		{ id: 0, building: "Loading ...", level: 3, location: 23 },
 		{ id: 1, building: "Loading ...", level: 2, location: 44 },
+		{ id: 2, building: "Loading ...", level: 1, location: 55 },
+		{ id: 3, building: "Loading ...", level: 0, location: 66 },
+		{ id: 4, building: "Loading ...", level: 0, location: 77 },
 	];
-	const [selected, setSelected] = useState(0);
 
-	const onClick = (vill) => {
-		setSelected(vill.id);
+	const [items, setItems] = useState(data);
+
+	const onRemove = (index) => {
+		setItems((items) => items.filter((item) => item.id !== index));
+		console.log("remove", index);
 	};
+
+	const onSortEnd = ({ oldIndex, newIndex }) => {
+		setItems(arrayMoveImmutable(items, oldIndex, newIndex));
+		console.log("sort", oldIndex, newIndex);
+	};
+
+	useEffect(() => {
+		console.log(items);
+	}, [items]);
+
 	return (
 		<>
 			<ContentBox>
 				<Typography variant="h5">Queue Building</Typography>
-				<MUITable
+				<MUIDraggableTable
 					header={header}
-					data={data}
-					handler={onClick}
-					selected={selected}
+					data={items}
+					onRemove={onRemove}
+					onSortEnd={onSortEnd}
+					useDragHandle
 				/>
 			</ContentBox>
 		</>
