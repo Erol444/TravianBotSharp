a2b999e76dae4cf134aff03c2574fe737def47ff
VINAGHOST <nguyentrungnhan87@gmail.com>
2022-04-25 21:35:49 +0700

    fix sleep task type stop bot pause

--- TbsCore/Tasks/LowLevel/ReopenDriver.cs
+++ TbsCore/Tasks/LowLevel/ReopenDriver.cs
@@ -15,29 +15,16 @@ namespace TbsCore.Tasks.LowLevel
 
         public override async Task<TaskRes> Execute(Account acc)
         {
+            StopFlag = false;
             acc.Wb.Close();
             string previousLog = "";
             do
             {
+                if (StopFlag) break;
                 await Task.Delay(1000);
                 var minutes = GetMinutes(acc);
                 if (minutes <= 0) break;
-                string log;
-                switch (acc.Status)
-                {
-                    case Status.Paused:
-                    case Status.Pausing:
-                        log = $"Chrome will reopen in {minutes} mins but account is paused, chrome won't be open until bot is resumed";
-                        break;
-
-                    case Status.Stopping:
-                        acc.Logger.Information("Account logout. Ignore reopen chrome");
-                        return TaskRes.Executed;
-
-                    default:
-                        log = $"Chrome will reopen in {minutes} mins";
-                        break;
-                }
+                string log = $"Chrome will reopen in {minutes} mins";
 
                 if (log != previousLog)
                 {
