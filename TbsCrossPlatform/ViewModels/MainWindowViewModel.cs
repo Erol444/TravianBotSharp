780249fe690a1067dfa43a8df8be1953d3f6c0e5
VINAGHOST <nguyentrungnhan87@gmail.com>
2022-05-18 14:58:45 +0700

    add button & icon

--- TbsCrossPlatform/ViewModels/MainWindowViewModel.cs
+++ TbsCrossPlatform/ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,6 @@ namespace TbsCrossPlatform.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public string Greeting => "TravianBotSharp test demo!";
     }
 }
