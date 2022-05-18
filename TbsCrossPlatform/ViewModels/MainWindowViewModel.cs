0c684143caaf58bf74ab49bdf56b2bf9935e4843
VINAGHOST <nguyentrungnhan87@gmail.com>
2022-05-18 15:39:45 +0700

    test chromedriver

--- TbsCrossPlatform/ViewModels/MainWindowViewModel.cs
+++ TbsCrossPlatform/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,42 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Avalonia.Interactivity;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using ReactiveUI;
 
 namespace TbsCrossPlatform.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "TravianBotSharp test demo!";
+        private bool browser = false;
+        private IWebDriver driver;
+
+        private bool Browser
+        {
+            set
+            {
+                browser = value;
+                this.RaisePropertyChanged(nameof(ContentButton));
+            }
+            get => browser;
+        }
+
+        public string ContentButton => Browser ? "Close chrome" : "Open chrome";
+
+        public void OnClickCommand()
+        {
+            if (!browser)
+            {
+                ChromeDriverService chromeService = ChromeDriverService.CreateDefaultService();
+                chromeService.HideCommandPromptWindow = true;
+                driver = new ChromeDriver(chromeService);
+                driver.Navigate().GoToUrl("https://github.com/Erol444/TravianBotSharp");
+                Browser = true;
+            }
+            else
+            {
+                driver.Quit();
+                Browser = false;
+            }
+        }
     }
 }
