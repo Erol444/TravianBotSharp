42b3d32aeb68e6d430d735c2efd1f080bd92dc6f
VINAGHOST <nguyentrungnhan87@gmail.com>
2022-05-20 11:42:42 +0700

    add sqlite & database context

---
+++ TbsCrossPlatform/Database/AccountContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TbsCrossPlatform.Database
+{
+    public class AccountContext : DbContext
+    {
+        public static string GetConnectionString() => new SqliteConnectionStringBuilder($"Data Source=TBS.db;Cache=Shared")
+        {
+            Mode = SqliteOpenMode.ReadWriteCreate,
+        }.ToString();
+
+        public AccountContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(GetConnectionString());
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
