49b4dc77e7f04f88df758a8ae23c3b32dffac91a
VINAGHOST <nguyentrungnhan87@gmail.com>
2022-05-21 23:27:41 +0700

    add tabs to main window

--- TbsCrossPlatform/Views/MainWindow.axaml.cs
+++ TbsCrossPlatform/Views/MainWindow.axaml.cs
@@ -1,12 +1,24 @@
-using Avalonia.Controls;
+using Avalonia;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using TbsCrossPlatform.ViewModels;
 
 namespace TbsCrossPlatform.Views
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
     {
         public MainWindow()
         {
             InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            ViewModel = new();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
         }
     }
 }
