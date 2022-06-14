fd482ce7022bdabb0be6a68270cf6a0f3bc749be
VINAGHOST <nguyentrungnhan87@gmail.com>
2022-06-14 18:29:34 +0700

    fix hero equip

--- TbsCore/Tasks/Others/HeroEquip.cs
+++ TbsCore/Tasks/Others/HeroEquip.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,62 +21,62 @@ namespace TbsCore.Tasks.Others
 
             acc.Hero.Items = HeroParser.GetHeroInventory(acc.Wb.Html);
             acc.Hero.Equipt = HeroParser.GetHeroEquipment(acc.Wb.Html);
-            //foreach (var use in Items)
-            //{
-            //    var (item, amount) = use;
 
-            //    var (category, _, _) = HeroHelper.ParseHeroItem(item);
-            //    if (category != HeroItemCategory.Resource && category != HeroItemCategory.Stackable && category != HeroItemCategory.NonStackable)
-            //    {
-            //        // Check if hero is at home
-            //        if (acc.Hero.Status != Hero.StatusEnum.Home)
-            //        {
-            //            acc.Logger.Warning("Hero isn't in home village");
-            //            // Wait for hero to come home
-            //            var nextExecute = acc.Hero.NextHeroSend > acc.Hero.HeroArrival ?
-            //                acc.Hero.NextHeroSend :
-            //                acc.Hero.HeroArrival;
+            foreach (var use in Items)
+            {
+                var (item, amount) = use;
 
-            //            var in5Min = DateTime.Now.AddMinutes(5);
-            //            if (nextExecute < in5Min) nextExecute = in5Min;
-            //            NextExecute = nextExecute;
-            //            return TaskRes.Retry;
-            //        }
-            //    }
+                var (category, _, _) = HeroHelper.ParseHeroItem(item);
+                if (category != HeroItemCategory.Resource && category != HeroItemCategory.Stackable && category != HeroItemCategory.NonStackable)
+                {
+                    // Check if hero is at home
+                    if (acc.Hero.Status != Hero.StatusEnum.Home)
+                    {
+                        acc.Logger.Warning("Hero isn't in home village");
+                        // Wait for hero to come home
+                        var nextExecute = acc.Hero.NextHeroSend > acc.Hero.HeroArrival ?
+                            acc.Hero.NextHeroSend :
+                            acc.Hero.HeroArrival;
 
-            //    string script = "var items = document.getElementById('itemsToSale');";
+                        var in5Min = DateTime.Now.AddMinutes(5);
+                        if (nextExecute < in5Min) nextExecute = in5Min;
+                        NextExecute = nextExecute;
+                        return TaskRes.Retry;
+                    }
+                }
 
-            //    switch (acc.AccInfo.ServerVersion)
-            //    {
-            //        case ServerVersionEnum.T4_5:
-            //            script += $"items.querySelector('div[class$=\"_{(int)item}\"]').click();";
-            //            break;
+                var itemNode = acc.Wb.Html.DocumentNode.Descendants("div").FirstOrDefault(x => x.HasClass($"item{(int)item}"));
+                itemNode = itemNode.ParentNode;
+                var elements = acc.Wb.Driver.FindElements(By.XPath(itemNode.XPath));
+                elements[0].Click();
+                var wait = new WebDriverWait(acc.Wb.Driver, TimeSpan.FromMinutes(1));
+                wait.Until(driver => driver.FindElements(By.Id("consumableHeroItem")).Count > 0);
+                acc.Wb.UpdateHtml();
 
-            //        case ServerVersionEnum.TTwars:
-            //            script += $"items.querySelector('div[class$=\"_{(int)item} \"]').click();";
-            //            break;
-            //    }
+                var form = acc.Wb.Html.GetElementbyId("consumableHeroItem");
+                var input = form.Descendants("input").FirstOrDefault();
+                var inputElements = acc.Wb.Driver.FindElements(By.XPath(input.XPath));
 
-            //    await DriverHelper.ExecuteScript(acc, script);
+                inputElements[0].SendKeys(Keys.Home);
+                inputElements[0].SendKeys(Keys.Shift + Keys.End);
+                inputElements[0].SendKeys($"{amount}");
 
-            //    // No amount specified, meaning we have already equipt the item
-            //    if (amount == 0) continue;
-            //    await Task.Delay(900);
-            //    acc.Wb.UpdateHtml();
-            //    var amountNode = acc.Wb.Html.GetElementbyId("amount");
-            //    if (amountNode == null) continue;
-            //    var amountElement = acc.Wb.Driver.FindElement(By.XPath(amountNode.XPath));
-            //    amountElement.SendKeys(Keys.Home);
-            //    amountElement.SendKeys(Keys.Shift + Keys.End);
-            //    amountElement.SendKeys($"{amount}");
+                var dialog = acc.Wb.Html.GetElementbyId("dialogContent");
+                var buttonWrapper = dialog.Descendants("div").FirstOrDefault(x => x.HasClass("buttonsWrapper"));
+                var buttonTransfer = buttonWrapper.Descendants("button").ToArray();
+                var buttonTransferElements = acc.Wb.Driver.FindElements(By.XPath(buttonTransfer[1].XPath));
+                buttonTransferElements[0].Click();
 
-            //    var okNode = acc.Wb.Html.DocumentNode.Descendants("button").FirstOrDefault(x => x.HasClass("ok"));
-            //    if (okNode == null) continue;
-            //    var okElement = acc.Wb.Driver.FindElement(By.XPath(okNode.XPath));
-            //    okElement.Click();
-            //    await Task.Delay(900);
-            //    HeroHelper.ParseHeroPage(acc);
-            //}
+                wait.Until(driver =>
+                {
+                    acc.Wb.UpdateHtml();
+                    var inventoryPageWrapper = acc.Wb.Html.DocumentNode.Descendants("div").FirstOrDefault(x => x.HasClass("inventoryPageWrapper"));
+                    return !inventoryPageWrapper.HasClass("loading");
+                });
+            }
+
+            acc.Hero.Items = HeroParser.GetHeroInventory(acc.Wb.Html);
+            acc.Hero.Equipt = HeroParser.GetHeroEquipment(acc.Wb.Html);
 
             return TaskRes.Executed;
         }
--- TbsWinformNet6/Views/NewVillagesUc.cs
+++ TbsWinformNet6/Views/NewVillagesUc.cs
@@ -1,4 +1,6 @@
-﻿using TbsCore.Helpers;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using TbsCore.Helpers;
 using TbsCore.Models.MapModels;
 using TbsCore.Models.VillageModels;
 using TbsCore.Tasks.Others;
@@ -6,6 +8,7 @@ using TbsCore.Tasks.Sim;
 using TbsWinformNet6.Helpers;
 using TbsWinformNet6.Interfaces;
 using TbsWinformNet6.Views.BaseViews;
+using Keys = OpenQA.Selenium.Keys;
 
 namespace TbsWinformNet6.Views
 {
@@ -143,7 +146,11 @@ namespace TbsWinformNet6.Views
         private async void button6_Click(object sender, EventArgs e)
         {
             var acc = GetSelectedAcc();
-            var task = new HeroEquip();
+            var items = new List<(Classificator.HeroItemEnum, int)>
+            {
+                new (Classificator.HeroItemEnum.Resource_Crop_0, 1),
+            };
+            var task = new HeroEquip() { Items = items };
             try
             {
                 await task.Execute(acc);
@@ -156,6 +163,45 @@ namespace TbsWinformNet6.Views
 
         private async void button7_Click(object sender, EventArgs e)
         {
+            try
+            {
+                var acc = GetSelectedAcc();
+                acc.Wb.UpdateHtml();
+                var item = acc.Wb.Html.DocumentNode.Descendants("div").FirstOrDefault(x => x.HasClass("item148"));
+                item = item.ParentNode;
+                var elements = acc.Wb.Driver.FindElements(By.XPath(item.XPath));
+                elements[0].Click();
+                var wait = new WebDriverWait(acc.Wb.Driver, TimeSpan.FromMinutes(1));
+                wait.Until(driver => driver.FindElements(By.Id("consumableHeroItem")).Count > 0);
+                acc.Wb.UpdateHtml();
+
+                var form = acc.Wb.Html.GetElementbyId("consumableHeroItem");
+                var input = form.Descendants("input").FirstOrDefault();
+                var inputElements = acc.Wb.Driver.FindElements(By.XPath(input.XPath));
+
+                inputElements[0].SendKeys(Keys.Home);
+                inputElements[0].SendKeys(Keys.Shift + Keys.End);
+                inputElements[0].SendKeys($"{1}");
+
+                var dialog = acc.Wb.Html.GetElementbyId("dialogContent");
+                var buttonWrapper = dialog.Descendants("div").FirstOrDefault(x => x.HasClass("buttonsWrapper"));
+                var buttonTransfer = buttonWrapper.Descendants("button").ToArray();
+                var buttonTransferElements = acc.Wb.Driver.FindElements(By.XPath(buttonTransfer[1].XPath));
+                buttonTransferElements[0].Click();
+
+                wait.Until(driver =>
+                {
+                    acc.Wb.UpdateHtml();
+                    var inventoryPageWrapper = acc.Wb.Html.DocumentNode.Descendants("div").FirstOrDefault(x => x.HasClass("inventoryPageWrapper"));
+                    return !inventoryPageWrapper.HasClass("loading");
+                });
+
+                _ = dialog;
+            }
+            catch (Exception ex)
+            {
+                _ = ex;
+            }
         }
     }
 }
