00732a73eed76d180b04d02f9fdc3009d0a37809
VINAGHOST <nguyentrungnhan87@gmail.com>
2022-06-15 22:59:10 +0700

    fix hero set point

--- TbsCore/Tasks/Others/HeroSetPoints.cs
+++ TbsCore/Tasks/Others/HeroSetPoints.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using TbsCore.Helpers;
 using TbsCore.Models.AccModels;
@@ -17,16 +20,45 @@ namespace TbsCore.Tasks.Others
 
             float sum = 0;
             for (int i = 0; i < 4; i++) sum += acc.Hero.Settings.Upgrades[i];
-            if (sum == 0)
-            {
-                // Upgrade points were not set. Set points to default
-                acc.Hero.Settings.Upgrades = new byte[4] { 2, 0, 0, 2 };
-                sum = 4;
-            }
-
             var points = acc.Hero.HeroInfo.AvaliblePoints;
+            var wait = new WebDriverWait(acc.Wb.Driver, TimeSpan.FromMinutes(1));
+            var pointUsed = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                var amount = Math.Ceiling(acc.Hero.Settings.Upgrades[i] * points / sum);
+                if (amount == 0) continue;
 
-            //await Driver
+                for (int j = 0; j < (int)amount; j++)
+                {
+                    {
+                        var attributesDiv = acc.Wb.Html.DocumentNode.Descendants("div").FirstOrDefault(x => x.HasClass("heroAttributes"));
+                        var pointButtons = attributesDiv.Descendants("button").Where(x => x.HasClass("plus")).ToArray();
+                        var elements = acc.Wb.Driver.FindElements(By.XPath(pointButtons[i].XPath));
+                        elements[0].Click();
+                    }
+                    wait.Until(driver =>
+                    {
+                        acc.Wb.UpdateHtml();
+                        var attributesDiv = acc.Wb.Html.DocumentNode.Descendants("div").FirstOrDefault(x => x.HasClass("heroAttributes"));
+                        if (attributesDiv == null) return false;
+                        var avaliblePoints = attributesDiv.Descendants("div").FirstOrDefault(x => x.HasClass("pointsAvailable"));
+                        if (avaliblePoints == null) return false;
+                        var currentPoint = (int)Parser.RemoveNonNumeric(avaliblePoints.InnerText);
+                        if (acc.Hero.HeroInfo.AvaliblePoints - pointUsed - currentPoint == 0) return false;
+                        pointUsed++;
+                        return true;
+                    });
+                }
+            }
+            var buttonElements = acc.Wb.Driver.FindElements(By.Id("savePoints"));
+            buttonElements[0].Click();
+            wait.Until(driver =>
+            {
+                var button = acc.Wb.Driver.FindElements(By.Id("savePoints"));
+                if (button.Count == 0) return false;
+                return !button[0].Enabled;
+            });
+            acc.Hero.HeroInfo = HeroParser.GetHeroAttributes(acc.Wb.Html);
             return TaskRes.Executed;
         }
     }
--- TbsWinformNet6/Views/NewVillagesUc.cs
+++ TbsWinformNet6/Views/NewVillagesUc.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium.Support.UI;
 using TbsCore.Helpers;
 using TbsCore.Models.MapModels;
 using TbsCore.Models.VillageModels;
+using TbsCore.Parsers;
 using TbsCore.Tasks.Others;
 using TbsCore.Tasks.Sim;
 using TbsWinformNet6.Helpers;
@@ -131,11 +132,9 @@ namespace TbsWinformNet6.Views
 
         private async void button5_Click(object sender, EventArgs e)
         {
-            var acc = GetSelectedAcc();
-            var task = new HeroSetPoints();
             try
             {
-                await task.Execute(acc);
+                await NavigationHelper.ToAuction(GetSelectedAcc(), NavigationHelper.AuctionTab.Bids);
             }
             catch (Exception ex)
             {
@@ -167,36 +166,14 @@ namespace TbsWinformNet6.Views
             {
                 var acc = GetSelectedAcc();
                 acc.Wb.UpdateHtml();
-                var item = acc.Wb.Html.DocumentNode.Descendants("div").FirstOrDefault(x => x.HasClass("item148"));
-                item = item.ParentNode;
-                var elements = acc.Wb.Driver.FindElements(By.XPath(item.XPath));
+                acc.Hero.HeroInfo = HeroParser.GetHeroAttributes(acc.Wb.Html);
+                acc.Hero.HeroArrival = DateTime.Now + HeroParser.GetHeroArrivalInfo(acc.Wb.Html);
+                
+                var attributesDiv = acc.Wb.Html.DocumentNode.Descendants("div").FirstOrDefault(x => x.HasClass("heroAttributes"));
+                var pointButtons = attributesDiv.Descendants("button").Where(x => x.HasClass("plus")).ToArray();
+                var elements = acc.Wb.Driver.FindElements(By.XPath(pointButtons[3].XPath));
                 elements[0].Click();
-                var wait = new WebDriverWait(acc.Wb.Driver, TimeSpan.FromMinutes(1));
-                wait.Until(driver => driver.FindElements(By.Id("consumableHeroItem")).Count > 0);
-                acc.Wb.UpdateHtml();
-
-                var form = acc.Wb.Html.GetElementbyId("consumableHeroItem");
-                var input = form.Descendants("input").FirstOrDefault();
-                var inputElements = acc.Wb.Driver.FindElements(By.XPath(input.XPath));
-
-                inputElements[0].SendKeys(Keys.Home);
-                inputElements[0].SendKeys(Keys.Shift + Keys.End);
-                inputElements[0].SendKeys($"{1}");
-
-                var dialog = acc.Wb.Html.GetElementbyId("dialogContent");
-                var buttonWrapper = dialog.Descendants("div").FirstOrDefault(x => x.HasClass("buttonsWrapper"));
-                var buttonTransfer = buttonWrapper.Descendants("button").ToArray();
-                var buttonTransferElements = acc.Wb.Driver.FindElements(By.XPath(buttonTransfer[1].XPath));
-                buttonTransferElements[0].Click();
-
-                wait.Until(driver =>
-                {
-                    acc.Wb.UpdateHtml();
-                    var inventoryPageWrapper = acc.Wb.Html.DocumentNode.Descendants("div").FirstOrDefault(x => x.HasClass("inventoryPageWrapper"));
-                    return !inventoryPageWrapper.HasClass("loading");
-                });
-
-                _ = dialog;
+                //savePoints
             }
             catch (Exception ex)
             {
