ac04886cb9229c12cf1cc87b788cf29087b9eef1
VINAGHOST <nguyentrungnhan87@gmail.com>
2022-07-27 17:45:55 +0700

    bind button and data

--- WPFUI/ViewModels/Tabs/Villages/BuildViewModel.cs
+++ WPFUI/ViewModels/Tabs/Villages/BuildViewModel.cs
@@ -12,31 +12,105 @@ namespace WPFUI.ViewModels.Tabs.Villages
 {
     public class BuildViewModel : ReactiveObject
     {
-        public ReactiveCommand<Unit, Unit> SingleBuildCommand { get; }
-        public ReactiveCommand<Unit, Unit> ResourceBuildCommand { get; }
-        public ReactiveCommand<Unit, Unit> PreBuildCommand { get; }
+        public BuildViewModel()
+        {
+            BuildCommand = ReactiveCommand.Create(BuildTask);
 
+            TopCommand = ReactiveCommand.Create(TopTask);
+            BottomCommand = ReactiveCommand.Create(BottomTask);
+            UpCommand = ReactiveCommand.Create(UpTask);
+            DownCommand = ReactiveCommand.Create(DownTask);
+            DeleteCommand = ReactiveCommand.Create(DeleteTask);
+            DeleteAllCommand = ReactiveCommand.Create(DeleteAllTask);
+            ImportCommand = ReactiveCommand.Create(ImportTask);
+            ExportCommand = ReactiveCommand.Create(ExportTask);
+        }
+
+        public void LoadData(int accountId)
+        {
+        }
+
+        private void BuildTask()
+        {
+        }
+
+        private void TopTask()
+        {
+        }
+
+        private void BottomTask()
+        {
+        }
+
+        private void UpTask()
+        {
+        }
+
+        private void DownTask()
+        {
+        }
+
+        private void DeleteTask()
+        {
+        }
+
+        private void DeleteAllTask()
+        {
+        }
+
+        private void ImportTask()
+        {
+        }
+
+        private void ExportTask()
+        {
+        }
+
+        public ReactiveCommand<Unit, Unit> BuildCommand { get; }
         public ReactiveCommand<Unit, Unit> TopCommand { get; }
         public ReactiveCommand<Unit, Unit> BottomCommand { get; }
         public ReactiveCommand<Unit, Unit> UpCommand { get; }
         public ReactiveCommand<Unit, Unit> DownCommand { get; }
         public ReactiveCommand<Unit, Unit> DeleteCommand { get; }
-        public ReactiveCommand<Unit, Unit> ClearCommand { get; }
+        public ReactiveCommand<Unit, Unit> DeleteAllCommand { get; }
         public ReactiveCommand<Unit, Unit> ImportCommand { get; }
         public ReactiveCommand<Unit, Unit> ExportCommand { get; }
 
         public ObservableCollection<BuildingInfo> Buildings { get; } = new();
         public ObservableCollection<CurrentlyBuildingInfo> CurrentlyBuildings { get; } = new();
         public ObservableCollection<QueueBuildingInfo> QueueBuildings { get; } = new();
+        public ObservableCollection<Building> SelectorBuildings { get; } = new();
 
-        private Building _singleBuilding;
+        private Building _currentBuilding;
 
-        public Building SingleBuilding
+        public Building CurrentBuilding
         {
-            get => _singleBuilding;
-            set => this.RaiseAndSetIfChanged(ref _singleBuilding, value);
+            get => _currentBuilding;
+            set => this.RaiseAndSetIfChanged(ref _currentBuilding, value);
         }
 
-        private
+        private Building _currentQueueBuilding;
+
+        public Building CurrentQueueBuilding
+        {
+            get => _currentQueueBuilding;
+            set => this.RaiseAndSetIfChanged(ref _currentQueueBuilding, value);
+        }
+
+        private Building _selectedBuilding;
+
+        public Building SelectedBuilding
+        {
+            get => _selectedBuilding;
+            set => this.RaiseAndSetIfChanged(ref _selectedBuilding, value);
+        }
+
+        private string _level;
+
+        public string Level
+        {
+            get => _level;
+            set => this.RaiseAndSetIfChanged(ref _level, value);
+        }
     }
 }
--- WPFUI/Views/Tabs/Villages/BuildPage.xaml.cs
+++ WPFUI/Views/Tabs/Villages/BuildPage.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Disposables;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,6 +27,89 @@ namespace WPFUI.Views.Tabs.Villages
         {
             ViewModel = new();
             InitializeComponent();
+
+            this.WhenActivated(d =>
+            {
+                this.BindCommand(ViewModel,
+                    vm => vm.BuildCommand,
+                    v => v.BuildButton)
+                .DisposeWith(d);
+
+                this.BindCommand(ViewModel,
+                    vm => vm.TopCommand,
+                    v => v.TopButton)
+                .DisposeWith(d);
+
+                this.BindCommand(ViewModel,
+                    vm => vm.BottomCommand,
+                    v => v.BottomButton)
+                .DisposeWith(d);
+
+                this.BindCommand(ViewModel,
+                    vm => vm.UpCommand,
+                    v => v.UpButton)
+                .DisposeWith(d);
+
+                this.BindCommand(ViewModel,
+                    vm => vm.DownCommand,
+                    v => v.DownButton)
+                .DisposeWith(d);
+
+                this.BindCommand(ViewModel,
+                    vm => vm.DeleteCommand,
+                    v => v.DeleteButton)
+                .DisposeWith(d);
+
+                this.BindCommand(ViewModel,
+                    vm => vm.DeleteAllCommand,
+                    v => v.DeleteAllButton)
+                .DisposeWith(d);
+
+                this.BindCommand(ViewModel,
+                    vm => vm.ImportCommand,
+                    v => v.ImportButton)
+                .DisposeWith(d);
+
+                this.BindCommand(ViewModel,
+                    vm => vm.ExportCommand,
+                    v => v.ExportButton)
+                .DisposeWith(d);
+
+                this.OneWayBind(ViewModel,
+                    vm => vm.Buildings,
+                    v => v.BuildingsGrid.ItemsSource)
+                .DisposeWith(d);
+
+                this.Bind(ViewModel,
+                    vm => vm.SelectedBuilding,
+                    v => v.BuildingsGrid.SelectedItem)
+                .DisposeWith(d);
+
+                this.OneWayBind(ViewModel,
+                    vm => vm.CurrentlyBuildings,
+                    v => v.CurrentGrid.ItemsSource)
+                .DisposeWith(d);
+
+                this.OneWayBind(ViewModel,
+                    vm => vm.QueueBuildings,
+                    v => v.QueueGrid.ItemsSource)
+                .DisposeWith(d);
+
+                this.Bind(ViewModel,
+                    vm => vm.CurrentQueueBuilding,
+                    v => v.CurrentGrid.SelectedItem)
+                .DisposeWith(d);
+
+                this.Bind(ViewModel,
+                    vm => vm.SelectedBuilding,
+                    v => v.BuildingsComboBox.SelectedItem)
+                .DisposeWith(d);
+
+                this.Bind(ViewModel,
+                    vm => vm.Level,
+                    v => v.LevelTextBox.Text)
+                .DisposeWith(d);
+            });
         }
     }
 }
