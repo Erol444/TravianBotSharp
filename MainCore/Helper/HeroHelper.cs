a5ee719b814f62acce467b4ac939eeef77db4643
VINAGHOST <nguyentrungnhan87@gmail.com>
2022-08-15 20:38:48 +0700

    add hero equip

---
+++ MainCore/Helper/HeroHelper.cs
@@ -0,0 +1,128 @@
+﻿using HtmlAgilityPack;
+using MainCore.Enums;
+using MainCore.Services;
+using OpenQA.Selenium;
+using System;
+
+#if TRAVIAN_OFFICIAL
+
+using TravianOfficialCore.FindElements;
+
+#elif TRAVIAN_OFFICIAL_HEROUI
+
+using TravianOfficialNewHeroUICore.Parsers;
+using TravianOfficialNewHeroUICore.FindElements;
+
+#elif TTWARS
+
+using TTWarsCore.Parsers;
+using TTWarsCore.FindElements;
+
+#endif
+
+namespace MainCore.Helper
+{
+    public static class HeroHelper
+    {
+        public static bool IsUsableWhenHeroAway(this HeroItemEnums item)
+        {
+            return item switch
+            {
+                HeroItemEnums.Ointment or HeroItemEnums.Scroll or HeroItemEnums.Bucket or HeroItemEnums.Tablets or HeroItemEnums.Book or HeroItemEnums.Artwork or HeroItemEnums.SmallBandage or HeroItemEnums.BigBandage or HeroItemEnums.Cage or HeroItemEnums.Wood or HeroItemEnums.Clay or HeroItemEnums.Iron or HeroItemEnums.Crop => true,
+                _ => false,
+            };
+        }
+
+        public static void ClickItem(IChromeBrowser chromeBrowser, HeroItemEnums item)
+        {
+            var doc = chromeBrowser.GetHtml();
+            var node = HeroPage.GetItemSlot(doc, (int)item);
+            if (node is null)
+            {
+                throw new Exception($"Cannot find item {item}");
+            }
+
+            var chrome = chromeBrowser.GetChrome();
+            var elements = chrome.FindElements(By.XPath(node.XPath));
+            if (elements.Count == 0)
+            {
+                throw new Exception($"Cannot find item {item}");
+            }
+
+            elements[0].Click();
+            var wait = chromeBrowser.GetWait();
+            if (item.IsUsableWhenHeroAway())
+            {
+                wait.Until(driver =>
+                {
+                    var html = new HtmlDocument();
+                    html.Load(driver.PageSource);
+                    var dialog = HeroPage.GetAmountBox(html);
+                    return dialog is not null;
+                });
+            }
+            else
+            {
+#if TRAVIAN_OFFICIAL_HEROUI
+                wait.Until(driver =>
+                {
+                    var html = new HtmlDocument();
+                    html.Load(driver.PageSource);
+                    var inventoryPageWrapper = html.DocumentNode.Descendants("div").FirstOrDefault(x => x.HasClass("inventoryPageWrapper"));
+                    return !inventoryPageWrapper.HasClass("loading");
+                });
+#else
+                wait.Until(driver => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
+#endif
+            }
+        }
+
+        public static void EnterAmount(IChromeBrowser chromeBrowser, int amount)
+        {
+            var doc = chromeBrowser.GetHtml();
+            var amountBox = HeroPage.GetAmountBox(doc);
+            if (amountBox is null)
+            {
+                throw new Exception("Cannot find amount box");
+            }
+            var chrome = chromeBrowser.GetChrome();
+            var amountInputs = chrome.FindElements(By.XPath(amountBox.XPath));
+            if (amountInputs.Count == 0)
+            {
+                throw new Exception("Cannot find amount box");
+            }
+            amountInputs[0].SendKeys(Keys.Home);
+            amountInputs[0].SendKeys(Keys.Shift + Keys.End);
+            amountInputs[0].SendKeys(amount.ToString());
+        }
+
+        public static void Confirm(IChromeBrowser chromeBrowser)
+        {
+            var doc = chromeBrowser.GetHtml();
+            var confirmButton = HeroPage.GetConfirmButton(doc);
+            if (confirmButton is null)
+            {
+                throw new Exception("Cannot find confirm button");
+            }
+            var chrome = chromeBrowser.GetChrome();
+            var elements = chrome.FindElements(By.XPath(confirmButton.XPath));
+            if (elements.Count == 0)
+            {
+                throw new Exception("Cannot find confirm button");
+            }
+            elements[0].Click();
+            var wait = chromeBrowser.GetWait();
+#if TRAVIAN_OFFICIAL_HEROUI
+                wait.Until(driver =>
+                {
+                    var html = new HtmlDocument();
+                    html.Load(driver.PageSource);
+                    var inventoryPageWrapper = html.DocumentNode.Descendants("div").FirstOrDefault(x => x.HasClass("inventoryPageWrapper"));
+                    return !inventoryPageWrapper.HasClass("loading");
+                });
+#else
+            wait.Until(driver => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
+#endif
+        }
+    }
+}
---
+++ MainCore/Tasks/Misc/HeroEquip.cs
@@ -0,0 +1,50 @@
+﻿using MainCore.Enums;
+using MainCore.Helper;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MainCore.Tasks.Misc
+{
+    public class HeroEquip : BotTask
+    {
+        public HeroEquip(int villageId, int accountId, List<(HeroItemEnums, int)> items) : base(accountId)
+        {
+            _villageId = villageId;
+            _items = items;
+        }
+
+        public override string Name => $"Hero equip {VillageId}";
+        private readonly List<(HeroItemEnums, int)> _items;
+
+        private readonly int _villageId;
+        public int VillageId => _villageId;
+
+        public override void Execute()
+        {
+            NavigateHelper.ToHeroInventory(ChromeBrowser);
+            using var context = ContextFactory.CreateDbContext();
+            if (VillageId != -1) NavigateHelper.SwitchVillage(context, ChromeBrowser, VillageId);
+            var heroStatus = context.Heroes.Find(AccountId).Status;
+
+            foreach ((var item, var amount) in _items)
+            {
+                if (heroStatus != HeroStatusEnums.Home && !item.IsUsableWhenHeroAway())
+                {
+                    return;
+                }
+
+                HeroHelper.ClickItem(ChromeBrowser, item);
+                if (amount == 1)
+                {
+                    continue;
+                }
+                else
+                {
+                    HeroHelper.EnterAmount(ChromeBrowser, amount);
+                    HeroHelper.Confirm(ChromeBrowser);
+                }
+                Thread.Sleep(600);
+            }
+        }
+    }
+}
