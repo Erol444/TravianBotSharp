c38d0b1370608594418c3809e864bd77ba709719
VINAGHOST <nguyentrungnhan87@gmail.com>
2022-10-28 21:07:34 +0700

    move to viewbase

--- UI/ViewModels/MainWindowViewModel.cs
+++ UI/ViewModels/MainWindowViewModel.cs
@@ -1,34 +1,11 @@
-using ReactiveUI;
-using System.Reactive.Disposables;
-
 namespace UI.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase, IActivatableViewModel
+    public class MainWindowViewModel : ViewModelBase
     {
-        public MainWindowViewModel()
+        public MainWindowViewModel() : base()
         {
-            this.WhenActivated(disposables =>
-            {
-                HandleActivation();
-
-                Disposable
-                    .Create(() => HandleDeactivation())
-                    .DisposeWith(disposables);
-            });
-        }
-
-        private void HandleActivation()
-        {
-            _ = 2;
-        }
-
-        private void HandleDeactivation()
-        {
-            _ = 2;
         }
 
         public string Greeting => "Welcome to Avalonia!";
-
-        public ViewModelActivator Activator { get; } = new();
     }
 }
--- UI/ViewModels/ViewModelBase.cs
+++ UI/ViewModels/ViewModelBase.cs
@@ -1,8 +1,30 @@
 using ReactiveUI;
+using System.Reactive.Disposables;
 
 namespace UI.ViewModels
 {
-    public class ViewModelBase : ReactiveObject
+    public class ViewModelBase : ReactiveObject, IActivatableViewModel
     {
+        public ViewModelBase()
+        {
+            this.WhenActivated(disposables =>
+            {
+                OnActived();
+
+                Disposable
+                    .Create(() => OnDeactived())
+                    .DisposeWith(disposables);
+            });
+        }
+
+        protected virtual void OnActived()
+        {
+        }
+
+        protected virtual void OnDeactived()
+        {
+        }
+
+        public ViewModelActivator Activator { get; } = new();
     }
 }
