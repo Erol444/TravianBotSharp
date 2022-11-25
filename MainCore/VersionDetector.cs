c15b6d4997f7f6e2a113e5e3d125409f74e55abc
VINAGHOST <nguyentrungnhan87@gmail.com>
2022-11-25 13:47:09 +0700

    Version detector

---
+++ MainCore/VersionDetector.cs
@@ -0,0 +1,23 @@
+﻿namespace MainCore
+{
+    public static class VersionDetector
+    {
+        public static bool IsTravianOfficial()
+        {
+#if TRAVIAN_OFFICIAL
+            return true;
+#else
+            return false;
+#endif
+        }
+
+        public static bool IsTTWars()
+        {
+#if TTWARS
+            return true;
+#else
+            return false;
+#endif
+        }
+    }
+}
