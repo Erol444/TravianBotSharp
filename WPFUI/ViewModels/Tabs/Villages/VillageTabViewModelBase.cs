36cb51b48402c377b9806b60655d336a703c78bc
VINAGHOST <nguyentrungnhan87@gmail.com>
2022-12-05 23:57:38 +0700

    villagetabviewmodelbase

---
+++ WPFUI/ViewModels/Tabs/Villages/VillageTabViewModelBase.cs
@@ -0,0 +1,50 @@
+﻿using ReactiveUI;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+namespace WPFUI.ViewModels.Tabs.Villages
+{
+    public abstract class VillageTabViewModelBase : ActivatableViewModelBase
+    {
+        public VillageTabViewModelBase()
+        {
+            this.WhenAnyValue(vm => vm._selectorViewModel.Account)
+                .Where(x => x is not null)
+                .Select(x => x.Id)
+                .ToProperty(this, vm => vm.AccountId, out _accountId);
+
+            this.WhenAnyValue(vm => vm._selectorViewModel.Village)
+                .Where(x => x is not null)
+                .Select(x => x.Id)
+                .ToProperty(this, vm => vm.VillageId, out _villageId);
+
+            _selectorViewModel.VillageChanged += OnVillageChanged;
+            Active += OnActive;
+        }
+
+        private void OnActive()
+        {
+            RxApp.TaskpoolScheduler.Schedule(() => Init(VillageId));
+        }
+
+        private void OnVillageChanged(int villageId)
+        {
+            RxApp.TaskpoolScheduler.Schedule(() => Init(villageId));
+        }
+
+        private readonly ObservableAsPropertyHelper<int> _accountId;
+
+        public int AccountId
+        {
+            get => _accountId.Value;
+        }
+
+        private readonly ObservableAsPropertyHelper<int> _villageId;
+
+        public int VillageId
+        {
+            get => _villageId.Value;
+        }
+    }
+}
