6a21cfc17c9cbbc8df918b0643010c0fc9816e1b
VINAGHOST <nguyentrungnhan87@gmail.com>
2023-02-08 22:11:56 +0700

    fix task method from add method from planmanager

--- MainCore/Services/Implementations/PlanManager.cs
+++ MainCore/Services/Implementations/PlanManager.cs
@@ -1,5 +1,4 @@
-﻿using MainCore.Helper;
-using MainCore.Models.Runtime;
+﻿using MainCore.Models.Runtime;
 using MainCore.Services.Interface;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
@@ -15,108 +14,106 @@ namespace MainCore.Services.Implementations
             _contextFactory = contextFactory;
         }
 
+        private PlanTask FixTask(int villageId, PlanTask task)
+        {
+            if (task.Type != Enums.PlanTypeEnums.General) return task;
+
+            using var context = _contextFactory.CreateDbContext();
+            // check wall
+            if (task.Building.IsWall())
+            {
+                var village = context.Villages.Find(villageId);
+                var tribe = context.AccountsInfo.Find(village.AccountId).Tribe;
+                var wall = tribe.GetWall();
+                if (task.Building != wall) task.Building = wall;
+                return task;
+            }
+
+            // check building can build muiltiple times (warehouse, ganary, ...)
+            if (task.Building.IsMultipleAllow())
+            {
+                var villageBuildings = context.VillagesBuildings.Where(x => x.VillageId == villageId).ToList();
+                var building = villageBuildings.Where(x => x.Type == task.Building).OrderByDescending(x => x.Level).FirstOrDefault();
+                if (building is not null)
+                {
+                    if (task.Location != building.Id && building.Level != building.Type.GetMaxLevel())
+                    {
+                        task.Location = building.Id;
+                    }
+                    return task;
+                }
+
+                var currentBuildings = context.VillagesCurrentlyBuildings.Where(x => x.VillageId == villageId).ToList();
+                var currentBuilding = currentBuildings.Where(x => x.Type == task.Building).OrderByDescending(x => x.Level).FirstOrDefault();
+                if (currentBuilding is not null)
+                {
+                    if (task.Location != currentBuilding.Location && currentBuilding.Level != currentBuilding.Type.GetMaxLevel())
+                    {
+                        task.Location = currentBuilding.Location;
+                    }
+                    return task;
+                }
+
+                var planTasks = GetList(villageId);
+                var planTask = planTasks.Where(x => x.Building == task.Building).OrderByDescending(x => x.Level).FirstOrDefault();
+                if (planTask is null) return task;
+
+                if (task.Location != planTask.Location && planTask.Level != planTask.Building.GetMaxLevel())
+                {
+                    task.Location = planTask.Location;
+                }
+                return task;
+            }
+
+            if (task.Building.IsResourceField())
+            {
+                var villageBuilding = context.VillagesBuildings.FirstOrDefault(x => x.VillageId == villageId && x.Id == task.Location);
+                // different type village ( 4446 import to 3337 for example )
+                // now i just ignore the different resource field
+                if (villageBuilding is null || villageBuilding.Type != task.Building) return null;
+                return task;
+            }
+            {
+                var villageBuildings = context.VillagesBuildings.Where(x => x.VillageId == villageId).ToList();
+                var building = villageBuildings.FirstOrDefault(x => x.Type == task.Building);
+                if (building is not null)
+                {
+                    if (task.Location != building.Id)
+                    {
+                        task.Location = building.Id;
+                    }
+                    return task;
+                }
+
+                var currentBuildings = context.VillagesCurrentlyBuildings.Where(x => x.VillageId == villageId).ToList();
+                var currentBuilding = currentBuildings.FirstOrDefault(x => x.Type == task.Building);
+                if (currentBuilding is not null)
+                {
+                    if (task.Location != currentBuilding.Location)
+                    {
+                        task.Location = currentBuilding.Location;
+                    }
+                    return task;
+                }
+
+                var planTasks = GetList(villageId);
+                var planTask = planTasks.FirstOrDefault(x => x.Building == task.Building);
+                if (planTask is null) return task;
+                if (task.Location != planTask.Location)
+                {
+                    task.Location = planTask.Location;
+                }
+            }
+            return task;
+        }
+
         public void Add(int villageId, PlanTask task)
         {
             Check(villageId);
+            task = FixTask(villageId, task);
+            if (task is null) return;
             lock (_objLocks[villageId])
             {
-                if (task.Type == Enums.PlanTypeEnums.General)
-                {
-                    using var context = _contextFactory.CreateDbContext();
-                    // check wall
-                    if (task.Building.IsWall())
-                    {
-                        var village = context.Villages.Find(villageId);
-                        var tribe = context.AccountsInfo.Find(village.AccountId).Tribe;
-                        var wall = tribe.GetWall();
-                        if (task.Building != wall) task.Building = wall;
-                    }
-
-                    // check building can build muiltiple times (warehouse, ganary, ...)
-                    if (task.Building.IsMultipleAllow())
-                    {
-                        var villageBuildings = context.VillagesBuildings.Where(x => x.VillageId == villageId).ToList();
-                        var building = villageBuildings.Where(x => x.Type == task.Building).OrderByDescending(x => x.Level).FirstOrDefault();
-                        if (building is null)
-                        {
-                            var currentBuildings = context.VillagesCurrentlyBuildings.Where(x => x.VillageId == villageId).ToList();
-                            var currentBuilding = currentBuildings.Where(x => x.Type == task.Building).OrderByDescending(x => x.Level).FirstOrDefault();
-                            if (currentBuilding is null)
-                            {
-                                var planTasks = GetList(villageId);
-                                var planTask = planTasks.Where(x => x.Building == task.Building).OrderByDescending(x => x.Level).FirstOrDefault();
-                                if (planTask is not null)
-                                {
-                                    if (task.Location != planTask.Location && planTask.Level != planTask.Building.GetMaxLevel())
-                                    {
-                                        task.Location = planTask.Location;
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                if (task.Location != currentBuilding.Location && currentBuilding.Level != currentBuilding.Type.GetMaxLevel())
-                                {
-                                    task.Location = currentBuilding.Location;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            if (task.Location != building.Id && building.Level != building.Type.GetMaxLevel())
-                            {
-                                task.Location = building.Id;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (task.Building.IsResourceField())
-                        {
-                            var villageBuilding = context.VillagesBuildings.Where(x => x.VillageId == villageId).FirstOrDefault(x => x.Id == task.Location);
-                            // different type village ( 4446 import to 3337 for example )
-                            // now i just ignore the different resource field
-                            if (villageBuilding is null || villageBuilding.Type != task.Building) return;
-                        }
-                        else
-                        {
-                            var villageBuildings = context.VillagesBuildings.Where(x => x.VillageId == villageId).ToList();
-                            var building = villageBuildings.FirstOrDefault(x => x.Type == task.Building);
-                            if (building is null)
-                            {
-                                var currentBuildings = context.VillagesCurrentlyBuildings.Where(x => x.VillageId == villageId).ToList();
-                                var currentBuilding = currentBuildings.FirstOrDefault(x => x.Type == task.Building);
-                                if (currentBuilding is null)
-                                {
-                                    var planTasks = GetList(villageId);
-                                    var planTask = planTasks.FirstOrDefault(x => x.Building == task.Building);
-                                    if (planTask is not null)
-                                    {
-                                        if (task.Location != planTask.Location)
-                                        {
-                                            task.Location = planTask.Location;
-                                        }
-                                    }
-                                }
-                                else
-                                {
-                                    if (task.Location != currentBuilding.Location)
-                                    {
-                                        task.Location = currentBuilding.Location;
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                if (task.Location != building.Id)
-                                {
-                                    task.Location = building.Id;
-                                }
-                            }
-                        }
-                    }
-                }
-
                 _tasksDict[villageId].Add(task);
             }
         }
