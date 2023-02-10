c8a583758b215e3da62d9b0ef8be81c425c02781
VINAGHOST <nguyentrungnhan87@gmail.com>
2023-02-10 12:50:30 +0700

    optimize sleep task

--- MainCore/Tasks/Misc/SleepTask.cs
+++ MainCore/Tasks/Misc/SleepTask.cs
@@ -1,5 +1,4 @@
 ﻿using FluentResults;
-using MainCore.Errors;
 using MainCore.Helper.Interface;
 using MainCore.Models.Database;
 using MainCore.Services.Interface;
@@ -16,6 +15,8 @@ namespace MainCore.Tasks.Misc
 
         private readonly IRestClientManager _restClientManager;
 
+        private DateTime _sleepEnd;
+
         public SleepTask(int accountId, CancellationToken cancellationToken = default) : base(accountId, cancellationToken)
         {
             _accessHelper = Locator.Current.GetService<IAccessHelper>();
@@ -25,83 +26,132 @@ namespace MainCore.Tasks.Misc
 
         public override Result Execute()
         {
+            _logManager.Information(AccountId, "Choosing next proxy ...");
+            var access = ChooseNextAccess();
+            if (access is null || IsForceSleep())
+            {
+                var sleepTime = GetSleepTime();
+                _sleepEnd = DateTime.Now.Add(sleepTime);
+                _logManager.Information(AccountId, $"No proxy vaild or force sleep is acitve. Bot will sleep {(int)sleepTime.TotalMinutes} mins");
+            }
+            else
+            {
+                var sleepTime = TimeSpan.FromSeconds(Random.Shared.Next(14 * 60, 16 * 60));
+                _sleepEnd = DateTime.Now.Add(sleepTime);
+                var proxyHost = string.IsNullOrEmpty(access.ProxyHost) ? "default" : access.ProxyHost;
+                _logManager.Information(AccountId, $"There is vaild proxy ({proxyHost}). Bot will sleep {(int)sleepTime.TotalMinutes} mins before switching to vaild proxy");
+            }
+
+            Sleep();
+            var status = _taskManager.GetAccountStatus(AccountId);
+            if (status != Enums.AccountStatus.Stopping && status != Enums.AccountStatus.Pausing)
+            {
+                WakeUp(access);
+            }
+            ExecuteAt = DateTime.Now.Add(GetWorkTime());
+            return Result.Ok();
+        }
+
+        private Access ChooseNextAccess()
+        {
             var context = _contextFactory.CreateDbContext();
-            var accesses = context.Accesses.Where(x => x.AccountId == AccountId).OrderBy(x => x.LastUsed);
+            var accesses = context.Accesses.Where(x => x.AccountId == AccountId).OrderBy(x => x.LastUsed).ToList();
             var currentAccess = accesses.Last();
+            accesses.Remove(currentAccess);
             var setting = context.AccountsSettings.Find(AccountId);
 
-            Access selectedAccess = null;
             foreach (var access in accesses)
             {
                 if (string.IsNullOrEmpty(access.ProxyHost))
                 {
-                    selectedAccess = access;
-                    break;
+                    return access;
                 }
 
                 var result = _accessHelper.IsValid(_restClientManager.Get(new(access)));
                 if (result)
                 {
-                    selectedAccess = access;
                     access.LastUsed = DateTime.Now;
                     context.SaveChanges();
-                    break;
+                    return access;
                 }
                 else
                 {
                     _logManager.Information(AccountId, $"Proxy {access.ProxyHost} is not working");
                 }
             }
-            if (selectedAccess is null || selectedAccess.Id == currentAccess.Id)
+
+            return null;
+        }
+
+        private bool IsForceSleep()
+        {
+            using var context = _contextFactory.CreateDbContext();
+            var setting = context.AccountsSettings.Find(AccountId);
+            return setting.IsSleepBetweenProxyChanging;
+        }
+
+        private TimeSpan GetSleepTime()
+        {
+            using var context = _contextFactory.CreateDbContext();
+            var setting = context.AccountsSettings.Find(AccountId);
+            (var min, var max) = (setting.SleepTimeMin, setting.SleepTimeMax);
+
+            var time = TimeSpan.FromSeconds(Random.Shared.Next(min * 60, max * 60));
+
+            return time;
+        }
+
+        private TimeSpan GetWorkTime()
+        {
+            using var context = _contextFactory.CreateDbContext();
+            var setting = context.AccountsSettings.Find(AccountId);
+            (var min, var max) = (setting.WorkTimeMin, setting.WorkTimeMax);
+
+            var time = TimeSpan.FromSeconds(Random.Shared.Next(min * 60, max * 60));
+
+            return time;
+        }
+
+        private void Sleep()
+        {
+            _chromeBrowser.Close();
+            int lastMinute = 0;
+
+            while (true)
             {
-                (var min, var max) = (setting.SleepTimeMin, setting.SleepTimeMax);
-
-                var time = TimeSpan.FromMinutes(Random.Shared.Next(min, max));
-                _chromeBrowser.Close();
-                _logManager.Information(AccountId, $"Bot is sleeping in {time.TotalMinutes} minute(s)");
-                while (time > TimeSpan.Zero)
+                if (CancellationToken.IsCancellationRequested)
                 {
-                    if (CancellationToken.IsCancellationRequested)
-                    {
-                        return Result.Fail(new Cancel());
-                    }
-                    Thread.Sleep(TimeSpan.FromSeconds(1));
-                    time -= TimeSpan.FromSeconds(1);
+                    _logManager.Information(AccountId, "Cancellation requested");
+                    return;
+                }
+                var timeRemaining = _sleepEnd - DateTime.Now;
+                if (timeRemaining < TimeSpan.Zero) return;
 
-                    if (time.Seconds == 0)
-                    {
-                        _logManager.Information(AccountId, $"Bot is sleeping in {time.TotalMinutes} minute(s)");
-                    }
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                var currentMinute = (int)timeRemaining.TotalMinutes;
+
+                if (lastMinute != currentMinute)
+                {
+                    _logManager.Information(AccountId, $"Chrome will reopen in {currentMinute} mins");
+                    lastMinute = currentMinute;
                 }
             }
-            else
-            {
-                _chromeBrowser.Close();
-                _logManager.Information(AccountId, $"Bot is sleeping in {3} minute(s)");
-                var time = TimeSpan.FromMinutes(3);
-                while (time > TimeSpan.Zero)
-                {
-                    if (CancellationToken.IsCancellationRequested)
-                    {
-                        return Result.Fail(new Cancel());
-                    }
-                    Thread.Sleep(TimeSpan.FromSeconds(1));
-                    time -= TimeSpan.FromSeconds(1);
+        }
 
-                    if (time.Seconds == 0)
-                    {
-                        _logManager.Information(AccountId, $"Bot is sleeping in {time.TotalMinutes} minute(s)");
-                    }
-                }
+        private void WakeUp(Access access)
+        {
+            using var context = _contextFactory.CreateDbContext();
+            // just use current access
+            if (access is null)
+            {
+                var accesses = context.Accesses.Where(x => x.AccountId == AccountId).OrderBy(x => x.LastUsed).ToList();
+                access = accesses.Last();
             }
-            _chromeBrowser.Setup(selectedAccess, setting);
+            var setting = context.AccountsSettings.Find(AccountId);
+            _chromeBrowser.Setup(access, setting);
             var currentAccount = context.Accounts.Find(AccountId);
             _chromeBrowser.Navigate(currentAccount.Server);
-            _taskManager.Add(AccountId, new LoginTask(AccountId), true);
-
-            var nextExecute = Random.Shared.Next(setting.SleepTimeMin, setting.SleepTimeMax);
-            ExecuteAt = DateTime.Now.AddMinutes(nextExecute);
-            return Result.Ok();
+            _taskManager.Add(AccountId, new LoginTask(AccountId), first: true);
         }
     }
 }
