723a37389351f13913edf9af1c8f1cf36a2381b8
VINAGHOST <nguyentrungnhan87@gmail.com>
2023-03-16 09:50:24 +0700

    update dorf

--- MainCore/Tasks/Update/UpdateBothDorf.cs
+++ MainCore/Tasks/Update/UpdateBothDorf.cs
@@ -1,6 +1,7 @@
 ﻿using FluentResults;
 using MainCore.Errors;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace MainCore.Tasks.Update
@@ -13,57 +14,54 @@ namespace MainCore.Tasks.Update
 
         public override Result Execute()
         {
+            var commands = new List<Func<Result>>();
             var url = _chromeBrowser.GetCurrentUrl();
-            var updateDorf1 = new UpdateDorf1(VillageId, AccountId, CancellationToken);
-            var updateDorf2 = new UpdateDorf2(VillageId, AccountId, CancellationToken);
             if (url.Contains("dorf2"))
             {
-                {
-                    var result = updateDorf2.Execute();
-                    if (result.IsFailed) return result.WithError(new Trace(Trace.TraceMessage()));
-                }
-                {
-                    var result = updateDorf1.Execute();
-                    if (result.IsFailed) return result.WithError(new Trace(Trace.TraceMessage()));
-                }
+                commands.Add(UpdateDorf2);
+                commands.Add(UpdateDorf1);
             }
             else if (url.Contains("dorf1"))
             {
-                {
-                    var result = updateDorf1.Execute();
-                    if (result.IsFailed) return result.WithError(new Trace(Trace.TraceMessage()));
-                }
-                {
-                    var result = updateDorf2.Execute();
-                    if (result.IsFailed) return result.WithError(new Trace(Trace.TraceMessage()));
-                }
+                commands.Add(UpdateDorf1);
+                commands.Add(UpdateDorf2);
             }
             else
             {
                 if (Random.Shared.Next(0, 100) > 50)
                 {
-                    {
-                        var result = updateDorf1.Execute();
-                        if (result.IsFailed) return result.WithError(new Trace(Trace.TraceMessage()));
-                    }
-                    {
-                        var result = updateDorf2.Execute();
-                        if (result.IsFailed) return result.WithError(new Trace(Trace.TraceMessage()));
-                    }
+                    commands.Add(UpdateDorf1);
+                    commands.Add(UpdateDorf2);
                 }
                 else
                 {
-                    {
-                        var result = updateDorf2.Execute();
-                        if (result.IsFailed) return result.WithError(new Trace(Trace.TraceMessage()));
-                    }
-                    {
-                        var result = updateDorf1.Execute();
-                        if (result.IsFailed) return result.WithError(new Trace(Trace.TraceMessage()));
-                    }
+                    commands.Add(UpdateDorf2);
+                    commands.Add(UpdateDorf1);
                 }
             }
+
+            foreach (var command in commands)
+            {
+                _logManager.Information(AccountId, $"[{GetName()}] Execute {command.Method.Name}");
+                var result = command.Invoke();
+                if (result.IsFailed) return result.WithError(new Trace(Trace.TraceMessage()));
+                if (CancellationToken.IsCancellationRequested) return Result.Fail(new Cancel());
+            }
             return Result.Ok();
+        }
+
+        private Result UpdateDorf1()
+        {
+            var updateDorf1 = new UpdateDorf1(VillageId, AccountId, CancellationToken);
+            var result = updateDorf1.Execute();
+            return result;
+        }
+
+        private Result UpdateDorf2()
+        {
+            var updateDorf2 = new UpdateDorf2(VillageId, AccountId, CancellationToken);
+            var result = updateDorf2.Execute();
+            return result;
         }
     }
 }
