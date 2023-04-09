a6368ad8e59e4d356c499d9fecf1584f12bd8341
VINAGHOST <nguyentrungnhan87@gmail.com>
2023-04-09 21:43:48 +0700

    ttwars train troop parser

--- TestProject/Parser/TroopTrain/TrainTroopParserTest.cs
+++ TestProject/Parser/TroopTrain/TrainTroopParserTest.cs
@@ -14,6 +14,7 @@ namespace TestProject.Parser.TroopTrain
     {
         private static readonly List<ITrainTroopParser> _instance = new(){
             new MainCore.Parsers.Implementations.TravianOfficial.TrainTroopParser(),
+            new MainCore.Parsers.Implementations.TTWars.TrainTroopParser(),
         };
 
         private static List<string> _version;
@@ -31,13 +32,16 @@ namespace TestProject.Parser.TroopTrain
         {
             _version = Enum.GetNames(typeof(VersionEnums)).ToList();
 
-            //Assert.AreEqual(_instance.Count, _version.Count);
+            Assert.AreEqual(_instance.Count, _version.Count);
         }
 
         [DataTestMethod]
         [DataRow(VersionEnums.TravianOfficial, BARRACK, 2)]
         [DataRow(VersionEnums.TravianOfficial, STABLE, 4)]
         [DataRow(VersionEnums.TravianOfficial, WORKSHOP, 2)]
+        [DataRow(VersionEnums.TTWars, BARRACK, 2)]
+        [DataRow(VersionEnums.TTWars, STABLE, 4)]
+        [DataRow(VersionEnums.TTWars, WORKSHOP, 2)]
         public void GetTroopNodesTest(VersionEnums version, string type, int expected)
         {
             var index = (int)version;
@@ -52,6 +56,9 @@ namespace TestProject.Parser.TroopTrain
         [DataRow(VersionEnums.TravianOfficial, BARRACK, 0, 22)]
         [DataRow(VersionEnums.TravianOfficial, STABLE, 0, 24)]
         [DataRow(VersionEnums.TravianOfficial, WORKSHOP, 0, 27)]
+        [DataRow(VersionEnums.TTWars, BARRACK, 0, 21)]
+        [DataRow(VersionEnums.TTWars, STABLE, 0, 23)]
+        [DataRow(VersionEnums.TTWars, WORKSHOP, 0, 27)]
         public void GetTroopTypeTest(VersionEnums version, string type, int location, int expected)
         {
             var index = (int)version;
@@ -67,6 +74,9 @@ namespace TestProject.Parser.TroopTrain
         [DataRow(VersionEnums.TravianOfficial, BARRACK, 0, 140, 150, 185, 60)]
         [DataRow(VersionEnums.TravianOfficial, STABLE, 0, 350, 450, 230, 60)]
         [DataRow(VersionEnums.TravianOfficial, WORKSHOP, 0, 950, 555, 330, 75)]
+        [DataRow(VersionEnums.TTWars, BARRACK, 0, 100, 130, 55, 30)]
+        [DataRow(VersionEnums.TTWars, STABLE, 0, 170, 150, 20, 40)]
+        [DataRow(VersionEnums.TTWars, WORKSHOP, 0, 950, 555, 330, 75)]
         public void GetTrainCostTest(VersionEnums version, string type, int location, int wood, int clay, int iron, int crop)
         {
             var index = (int)version;
@@ -82,9 +92,12 @@ namespace TestProject.Parser.TroopTrain
         }
 
         [DataTestMethod]
-        [DataRow(VersionEnums.TravianOfficial, BARRACK, 0, 191)]
-        [DataRow(VersionEnums.TravianOfficial, STABLE, 0, 328)]
-        [DataRow(VersionEnums.TravianOfficial, WORKSHOP, 0, 1121)]
+        [DataRow(VersionEnums.TravianOfficial, BARRACK, 0, 191_000)]
+        [DataRow(VersionEnums.TravianOfficial, STABLE, 0, 328_000)]
+        [DataRow(VersionEnums.TravianOfficial, WORKSHOP, 0, 1_121_000)]
+        [DataRow(VersionEnums.TTWars, BARRACK, 0, 7_024)]
+        [DataRow(VersionEnums.TTWars, STABLE, 0, 9_186)]
+        [DataRow(VersionEnums.TTWars, WORKSHOP, 0, 33_771)]
         public void GetTrainTimeTest(VersionEnums version, string type, int location, int expected)
         {
             var index = (int)version;
@@ -93,13 +106,16 @@ namespace TestProject.Parser.TroopTrain
 
             var nodes = _instance[index].GetTroopNodes(_doc);
             var actual = _instance[index].GetTrainTime(nodes[location]);
-            Assert.AreEqual(TimeSpan.FromSeconds(expected), actual);
+            Assert.AreEqual(TimeSpan.FromMilliseconds(expected), actual);
         }
 
         [DataTestMethod]
         [DataRow(VersionEnums.TravianOfficial, BARRACK, 0)]
         [DataRow(VersionEnums.TravianOfficial, STABLE, 0)]
         [DataRow(VersionEnums.TravianOfficial, WORKSHOP, 0)]
+        [DataRow(VersionEnums.TTWars, BARRACK, 0)]
+        [DataRow(VersionEnums.TTWars, STABLE, 0)]
+        [DataRow(VersionEnums.TTWars, WORKSHOP, 0)]
         public void GetInputBoxTest(VersionEnums version, string type, int location)
         {
             var index = (int)version;
@@ -115,6 +131,9 @@ namespace TestProject.Parser.TroopTrain
         [DataRow(VersionEnums.TravianOfficial, BARRACK, 0, 108)]
         [DataRow(VersionEnums.TravianOfficial, STABLE, 0, 86)]
         [DataRow(VersionEnums.TravianOfficial, WORKSHOP, 0, 51)]
+        [DataRow(VersionEnums.TTWars, BARRACK, 0, 24_736)]
+        [DataRow(VersionEnums.TTWars, STABLE, 0, 18_910)]
+        [DataRow(VersionEnums.TTWars, WORKSHOP, 0, 3_518)]
         public void GetMaxAmountTest(VersionEnums version, string type, int location, int expected)
         {
             var index = (int)version;
@@ -130,6 +149,9 @@ namespace TestProject.Parser.TroopTrain
         [DataRow(VersionEnums.TravianOfficial, BARRACK)]
         [DataRow(VersionEnums.TravianOfficial, STABLE)]
         [DataRow(VersionEnums.TravianOfficial, WORKSHOP)]
+        [DataRow(VersionEnums.TTWars, BARRACK)]
+        [DataRow(VersionEnums.TTWars, STABLE)]
+        [DataRow(VersionEnums.TTWars, WORKSHOP)]
         public void GetTrainButtonTest(VersionEnums version, string type)
         {
             var index = (int)version;
